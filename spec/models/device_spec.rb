a7ada0221e5639afaa46e33645410780316ce121
kholtzman5610 <kassidy.holtzman@outlook.com>
2020-05-23 14:58:51 -0400

    add device_rspec

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Device, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Device.new(device_name: "gears", game: FactoryBot.create(:game))}
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a device_name" do
+    subject.device_name=nil
+    expect(subject).to_not be_valid
+  end
+    it "is not valid without game_id" do
+      subject.game_id=nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid with an invalid game_id" do
+      subject.game_id=5555
+      expect(subject).to_not be_valid
+    end
 end
